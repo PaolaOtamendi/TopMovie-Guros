186c7c2c8d2024c8cfd573b3dde5cff9bed20927
PaolaOtamendi <paola_otamendi23@hotmail.com>
2024-06-21 17:23:42 -0600

    fix cambios en vistas de pantallas no detectado

--- src/componentes/Moviecard.jsx
+++ src/componentes/Moviecard.jsx
@@ -1,42 +1,58 @@
 import useObtenerGeneros from "../servicios/generos";
-import "./moviecard.css"
+import "./moviecard.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
 
-
 const Moviecard = ({ movie, onClick }) => {
-    const generos = useObtenerGeneros();
+  const generos = useObtenerGeneros();
 
-    const nombresGeneros = (ids) => {
-        return ids.map(id => {
-            const genero = generos.find(g => g.id === id);
-            return genero ? genero.name : 'Desconocido';
-        }).slice(0,3);
-    };
+  const nombresGeneros = (ids) => {
+    return ids
+      .map((id) => {
+        const genero = generos.find((g) => g.id === id);
+        return genero ? genero.name : "Desconocido";
+      })
+      .slice(0, 3);
+  };
 
-    return(
+  return (
     <div className="movie-card" onClick={onClick}>
-        <div className="contain-image">
-        <img className="image" src={`http://image.tmdb.org/t/p/w300/${movie.poster_path}`} alt={''} />
-        </div>
+      <div className="contain-image">
+        <img
+          className="image"
+          src={`http://image.tmdb.org/t/p/w300/${movie.poster_path}`}
+          alt=""/>
         <div className="movie-stars">
-            <p>{movie.vote_average.toFixed(1)}
-            <FontAwesomeIcon className="icon" icon={faStar} style={{color: "#e5004d",}} />
-            </p>
+          <p>
+            {movie.vote_average.toFixed(1)}
+            <FontAwesomeIcon
+              className="icon"
+              icon={faStar}
+              style={{ color: "#e5004d" }}/>
+          </p>
         </div>
-        <div className="contain-text">
+      </div>
+      <div className="contain-text">
         <div className="contain-titles">
-            <p className="movie-name">{movie.title}</p>
-            <p className="movie-id">{movie.id}</p>
+          <p className="movie-name">{movie.title}</p>
+          <p className="movie-id">{movie.id}</p>
+          <div>
+            <p className="title-id">¿De qué trata?</p>
+            <p className="movie-des">{movie.overview}</p>
+          </div>
         </div>
-        <div className="movie-generos">
-        {nombresGeneros(movie.genre_ids).map((genero, index) => (
-                    <button key={index} className="genre-button">{genero}</button>
-                ))}
+        <div className="contain-genave">
+          <div className="movie-generos">
+            {nombresGeneros(movie.genre_ids).map((genero, index) => (
+              <button key={index} className="genre-button">
+                {genero}
+              </button>
+            ))}
+          </div>
         </div>
-        </div>
+      </div>
     </div>
-    );
+  );
 };
 
 export default Moviecard;
